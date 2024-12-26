263ad7110b7384a6d48fc94e5ee2de2c8228a8dd
San <96554849+sandrayaquelineibarra@users.noreply.github.com>
2024-12-26 13:48:11 -0300

    Add files via upload

---
+++ desafio16.py
@@ -0,0 +1,6 @@
+base = float(input("Ingrese la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+
+area = (base * altura) / 2
+
+print(f"El área del triángulo es: {area}")
